4201f49bd133e0a233c618e72a1ecffc9a00270e
cityblack <690931381@qq.com>
2017-12-30 20:12:40 +0800

    change the machenizition of the framework

--- Impact3.py
+++ Impact3.py
@@ -1,14 +1,14 @@
-import threading
 import time
-import re
-import typing
+import os
+
+from importlib import import_module
+from collections.abc import Sequence
+from utils import TimeoutThreading
 from base import DMWrapper
-from supporters.dogfeed import SupportDogFeedTeamwork
-from utils import check_methods, specific_check, wrap_callbacks, false_to_retry, wrap_run, index_super
+from utils import check_methods
 from functools import partial
 from exceptions import InvalidCheckMethod, NoPageListFound
 from logger import logger
-from collections.abc import Sequence, Iterable
 
 
 class Impact3(DMWrapper):
@@ -21,11 +21,13 @@ class Impact3(DMWrapper):
         self.pages = []
         self.order_list = []
         self.callbacks = {}
-        self.child_tasks = []
         self.module_name = ''
         self.temp_result = ''
-        for fl in check_methods['base'].values():
+        self.child_tasks = []
+        for fl in check_methods.get('base', {}).values():
+            _ = fl[0].__name__
             fl[0] = partial(fl[0], self)
+            fl[0].__name__ = _
 
     def bind_callback(self, factory):
         if not self.pages:
@@ -34,39 +36,67 @@ class Impact3(DMWrapper):
             f_bound = self.callbacks.setdefault(self.module_name, {})
             f_bound[page] = partial(getattr(factory, page, None), self.factory_i)
 
+    def dispatch_run(self, func, timeout):
+        if timeout:
+            result = self.gen_threading(func, timeout=timeout)
+            result = result if result is not None else False
+        else:
+            result = func()
+        return result
+
     def check_page(self, page, retry_times=None, use_callback=None, fail_to_check=None, timeout=None, binding=None):
         """当将callback传递为None时不会调用callback, 否则一定会在result为真的时候调用它对应的callback"""
-        func, *others = check_methods.get(self.module_name).get('check_' + page) or check_methods['base'].get('check_' + page)
-        packed = zip([retry_times, use_callback, fail_to_check, timeout, binding], others)
+        try:
+            func, *others = check_methods.get(self.module_name).get('check_' + page) or check_methods['base'].get('check_' + page)
+        except TypeError as e:
+            raise InvalidCheckMethod('{}页面的check函数无法找到, 错误原因:{}'.format(page, str(e)))
+        if not func:
+            raise InvalidCheckMethod('{}页面的check函数无法找到, 错误原因未知'.format(page))
+        packed = zip([retry_times, use_callback, fail_to_check, timeout, binding], others[:5])
         retry_times, use_callback, fail_to_check, timeout, binding = map(lambda x: x[0] if x[0] is not None else x[1], packed)
-        result = func()
+        result = self.dispatch_run(func, timeout)
         while not result and retry_times:
             logger.info('check_' + page + '的检测失败, 重新检测, 剩余重试次数为' + str(retry_times))
             time.sleep(0.3)
-            result = func()
+            result = self.dispatch_run(func, timeout)
             retry_times -= 1
         if result and use_callback:
             callback = self.callbacks.get(self.module_name, {}).get(page)
             _ = callback() if callback else ''
             logger.info('回调执行成功, 返回值为' + str(_))
-        if not result and isinstance(fail_to_check, str):
-            self.check_page(fail_to_check, 0, False, list(), -1, list()) if fail_to_check[0] == '-' else \
-                self.check_page(fail_to_check, 0, True, list(), -1, list())
-        elif not result:
+        if not result and fail_to_check == 'all':
+            for p in self.pages:
+                self.check_page(p, 0, True, list(), None, list())
+        elif callable(fail_to_check):
+            fail_to_check()
+        elif not result and isinstance(fail_to_check, str):
+            self.check_page(fail_to_check, 0, False, list(), None, list()) if fail_to_check[0] == '-' else \
+                self.check_page(fail_to_check, 0, True, list(), None, list())
+        elif not result and isinstance(fail_to_check, Sequence):
             for f_checker in fail_to_check:
-                self.check_page(f_checker, 0, False, list(), -1, list()) if fail_to_check[0] == '-' else\
-                    self.check_page(fail_to_check, 0, True, list(), -1, list())
+                if callable(f_checker):
+                    r = f_checker()
+                else:
+                    r = self.check_page(f_checker, 0, False, list(), -1, list()) if fail_to_check[0] == '-' else \
+                        self.check_page(fail_to_check, 0, True, list(), -1, list())
+                if r:
+                    break
         return result
-        raise InvalidCheckMethod('the check method of Page %s  can\'t be found' % page)
 
-    def gen_threading(self, func, *args, name=None, **kwargs):
-        if self.alive:
-            thread = threading.Thread(target=func, args=args, kwargs=kwargs, name=name)
+    def gen_threading(self, func, args=(), kwargs=None, name=None, timeout=None, ):
+        name = func.__name__ if not name else name
+        if self.alive and self.factory_i.over:
+            thread = TimeoutThreading(target=func, args=args, kwargs=kwargs, name=name)
             thread.setDaemon(True)
             thread.start()
-            self.child_tasks.append(thread)
+            thread.join(timeout)
+            result = thread.get_result()
+            thread.raise_error(TimeoutError)
+            logger.error('执行{}的时候时间超时'.format(name))
+            return result
 
     def start(self):
+        self.load_all_factories() if not self.factories else None
         while self.alive:
             for factory in self.factories:
                 self.pages = factory.page_list[::]
@@ -74,16 +104,28 @@ class Impact3(DMWrapper):
                 self.factory_i = factory(self)
                 if factory.use_check_method:
                     for checker in check_methods[self.module_name].values():
+                        _ = checker[0].__name__
                         checker[0] = partial(checker[0], self.factory_i)
+                        checker[0].__name__ = _
                 if factory.use_callback:
                     self.bind_callback(factory)
                 with self.factory_i as f:
                     f.run()
             self.alive = False
 
+    def load_all_factories(self):
+        f_p = os.path.join(os.path.dirname(__file__), 'supporters')
+        f_n = [n[:-3] for n in os.listdir(f_p) if n.endswith('.py') and n != '__init__.py']
+        for f in f_n:
+            maps = vars(import_module('supporters.' + f))
+            base = maps['BaseFactory']
+            for v in maps.values():
+                if isinstance(v, type) and v.__bases__[0] == base:
+                    self.factories.append(v)
+
 
 if __name__ == '__main__':
-    impact3 = Impact3([SupportDogFeedTeamwork])
+    impact3 = Impact3()
     impact3.start()
     # a.compare_color(1210, 676, 'fd8a82')
     print()
--- utils.py
+++ utils.py
@@ -3,71 +3,40 @@ import win32gui
 import functools
 import os
 import time
+import ctypes
+import inspect
+import threading
 
 from logger import logger
 __author__ = '别用铅笔写爱'
 __date__ = '2017/12/21 0021 上午 2:54'
 check_methods = {}
-broken_down = 0
 
 
-def wrap_run(self):
-    def outer(func):
-        @functools.wraps(func)
-        def inner(*args, **kwargs):
-            global broken_down
-            if broken_down > 3:
-                self.over = True
-                broken_down = 0
-            while not self.over:
-                func(*args, **kwargs)
-            return
-        return inner
-    return outer
-
-
-def index_super(li, em):
-    try:
-        return li.index(em)
-    except:
+def func_error_wrapper(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         try:
-            return li.index(em[:-2])
-        except:
-            try:
-                return li.index(em+'?')
-            except:
-                return -1
-
-
-def false_to_retry(retry=5, broken=3):
-    def outer(func):
-        def inner(*args, **kwargs):
-            global broken_down
-            nonlocal retry
             result = func(*args, **kwargs)
-            while not result and retry > 0 and result is not None:
-                logger.info('尝试重新检查页面{}, 剩余次数{}'.format(args[-1], str(retry)))
-                result = func(*args, **kwargs)
-                retry -= 1
-                time.sleep(0.5)
-            retry = 5 if result else retry
-            if retry == 0:
-                broken_down += 1
+        except Exception as e:
+            logger.error('执行函数{}时出现了异常, 错误信息是{}'.format(func.__name__, str(e)), exc_info=True)
+        else:
             return result
-        return inner
-    return outer
+    return wrapper
 
 
-def page_checker_register(retry_times=0, use_callback=True, fail_to_check=list(), timeout=-1, binding=list()):
+def page_checker_register(retry_times=0, use_callback=True, fail_to_check=list(), timeout=None, binding=list()):
     """
-    装饰后的函数将会被注册并绑定。
+    装饰后的函数/方法将会被注册并绑定。
     :param retry_times: 失败后重试次数
     :param use_callback: 当成功的时候是否使用回调函数
-    :param fail_to_check: 字符串列表或字符串, 当失败的时候检查那些页面，直到成功为止, 字符串all代表全部检查, None代表不检查, 列表代表顺序检查.
-            默认执行回调, 如果不想执行回调需要在每个字符串前加上 '-'
+    :param fail_to_check: 传递为页面名称的字符串或方法/函数或包含两者的列表, 当失败的时候检查那些页面，
+            直到其中一次检查成功或者列表循环到尽头才会终止检查
+            默认执行回调, 如果不想执行回调需要在每个字符串前加上 '-', 字符串的方式不能精确控制每个函数, 
+            可以通过传递 函数/方法指针 的列表实现精确控制或者自定义的逻辑
     :param timeout: 检查最长时间，如果超时会抛出一个超时错误
     :param binding: 绑定函数。会按照列表的顺序执行所有里面的函数
-    :return:
+    :return: 一个被注册后的函数/方法
     """
     def outer(fun):
         def bool_dispatcher(f):
@@ -96,37 +65,50 @@ def page_checker_register(retry_times=0, use_callback=True, fail_to_check=list()
     return outer
 
 
-def specific_check(self, page, func):
-    """特殊的检查, 这个检查可以同时检查这个页面和下个页面, 直到无法遇到特殊检查"""
-    if func is None:
-        return
-    index = self.pages.index(page) or 0
+class TimeoutThreading(threading.Thread):
+    """
+    调用 raise_error 方法, 可以强行结束线程
+    可以获得函数执行的结果
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._result = None
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        time.sleep(0.3)
-        result = func(*args, **kwargs)
-        if not result:
-            next_check = self.check_page(self.pages[index + 1])
-            if next_check:
-                logger.info('特殊页面不存在, 当前页面是', self.page)
-                return True
-        if result:
-            logger.info('特殊页面检查成功', str(func))
-        return result
-    return wrapper
+    def _get_pid(self):
+        if not self.is_alive():
+            logger.error(self.name + '获得pid失败, 因为线程已经结束')
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for tid, obj in threading._active.items():
+            if obj is self:
+                self._thread_id = tid
+                return tid
+        logger.error(self.name + '无法找到对应的pid')
+
+    def run(self):
+        try:
+            if self._target:
+                self._result = self._target(*self._args, **self._kwargs)
+        finally:
+            del self._target, self._args, self._kwargs
+
+    def raise_error(self, error_type=Exception):
+        _async_raise(self._get_pid(), error_type)
+
+    def get_result(self):
+        return self._result
 
 
-def wrap_callbacks(page, func):
-    if not func:
-        return None
-
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        func(*args, **kwargs)
-        logger.info('{}页面的回调函数执行成功'.format(page))
-        return func
-    return wrapper
+@func_error_wrapper
+def _async_raise(tid, error_type):
+    if not inspect.isclass(error_type):
+        raise TypeError('只能传递错误的类')
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(error_type))
+    if res == 0:
+        raise ValueError('无效的tid: {}'.format(tid))
+    elif res != 1:
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
+        raise SystemError('对{}抛出异常失败'.format(tid))
 
 
 
